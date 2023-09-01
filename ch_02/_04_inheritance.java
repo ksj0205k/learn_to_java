97e5dd42bada5c8729335fa41875052788d9035f
ksj0205k <ksj020519@gmail.com>
2023-09-01 19:53:59 +0900

    first commit

---
+++ ch_02/_04_inheritance.java
@@ -0,0 +1,68 @@
+package ch_02;
+
+interface Predator {
+    String getFood();
+
+    default void printFood() {
+        System.out.printf("my food is %s\n", getFood());
+    }
+}
+
+interface Barkable {
+    void bark();
+}
+
+interface BarkablePredator extends Predator, Barkable {
+}
+
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Tiger extends Animal implements Predator, Barkable {
+    public String getFood() {
+        return "apple";
+    }
+
+    public void bark() {
+        System.out.println("어흥");
+    }
+}
+
+class Lion extends Animal implements BarkablePredator {
+    public String getFood() {
+        return "banana";
+    }
+
+    public void bark() {
+        System.out.println("으르렁");
+    }
+}
+
+class ZooKeeper {
+    void feed(Predator predator) {
+        System.out.println("feed " + predator.getFood());
+    }
+}
+
+class Bouncer {
+    void barkAnimal(Barkable animal) {
+        animal.bark();
+    }
+}
+
+
+public class _04_inheritance {
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+        Lion lion = new Lion();
+
+        Bouncer bouncer = new Bouncer();
+        bouncer.barkAnimal(tiger);
+        bouncer.barkAnimal(lion);
+    }
+}
